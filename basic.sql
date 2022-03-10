d572995811fbf205fe0acda0a9addeeaf4a6477b
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-09 23:03:18 -0300

    Adicionar solução exercício 3 - Control which rows are retrieved

--- basic.sql
+++ basic.sql
@@ -2,4 +2,8 @@
 SELECT * FROM cd.facilities;
 
 /* exercício 2 - Retrieve specific columns from a table */
-SELECT name, membercost FROM cd.facilities
+SELECT name, membercost FROM cd.facilities;
+
+/* exercício 3 - Control which rows are retrieved */
+SELECT * FROM cd.facilities
+WHERE membercost >0;
