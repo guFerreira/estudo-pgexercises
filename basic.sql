4de1ce9342e2b1f3ee878f1ceae4aae841f7bbf2
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-09 22:59:13 -0300

    Adicionar solução exercício 2 - Retrieve specific columns from a table

--- basic.sql
+++ basic.sql
@@ -1,2 +1,5 @@
 /* exercício 1 - Retrieve everything from a table */
 SELECT * FROM cd.facilities;
+
+/* exercício 2 - Retrieve specific columns from a table */
+SELECT name, membercost FROM cd.facilities
