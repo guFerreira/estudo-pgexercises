67beb2db6da1a0888f2f44c8e7c497696d8290df
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-09 23:18:29 -0300

    Adicionar solução exercício 4 - Control which rows are retrieved - part 2

--- basic.sql
+++ basic.sql
@@ -7,3 +7,19 @@ SELECT name, membercost FROM cd.facilities;
 /* exercício 3 - Control which rows are retrieved */
 SELECT * FROM cd.facilities
 WHERE membercost >0;
+
+/* exercício 4 - Control which rows are retrieved - part 2 */
+SELECT facid, name, membercost, monthlymaintenance
+FROM cd.facilities
+WHERE membercost < monthlymaintenance/50 
+	and membercost > 0
+
+
+/* exercício 5 - Basic string searches */
+/* exercício 6 - Matching against multiple possible values */
+/* exercício 7 - Classify results into buckets */
+/* exercício 8 - Working with dates */
+/* exercício 9 - Removing duplicates, and ordering results */
+/* exercício 10 - Combining results from multiple queries */
+/* exercício 11 - Simple aggregation */
+/* exercício 12 - More aggregation */
